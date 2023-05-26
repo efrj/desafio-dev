53e29942ed5235bd1423d26cac7bc7960c83584c
Edivaldo Reis <efrj@hotmail.com>
2023-05-26 10:40:20 -0300

    Updated backend config tests

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   root 'transactions#index'
   resources :uploads, only: [:new, :create]
   resources :transactions, only: [:index]
