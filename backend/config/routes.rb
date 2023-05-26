d466f281f99f7a2f55e3443bc83c7a9e6a413bc3
Edivaldo Reis <efrj@hotmail.com>
2023-05-26 11:48:19 -0300

    Updated routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   root 'transactions#index'
   resources :uploads, only: [:new, :create]
   resources :transactions, only: [:index]
